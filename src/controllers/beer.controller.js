ad8268f9e86f459b1569d41dcfe800d99d2caf16
ygorxds <ygordesa2010@hotmail.com>
2023-07-17 19:53:55 -0300

    feat: implementation business rule in the project

--- src/controllers/beer.controller.js
+++ src/controllers/beer.controller.js
@@ -42,7 +42,45 @@ const BeerController = {
         }
       
         return res.status(200).json({ msg: 'beer deleted' });
+      },
+
+      async findBeerStyleByTemperature(req, res) {
+        const { temperature } = req.body;
+      
+        let beers = await knex('Beers').select('*');
+        let selectedBeerStyle = null;
+      
+        let closestTemperatureDiff = Infinity;
+        let sortedBeerStyles = [];
+      
+        beers.forEach((beer) => {
+          const avgTemperature = (beer.tempMax + beer.tempMin) / 2;
+          const temperatureDiff = Math.abs(temperature - avgTemperature);
+      
+          if (temperatureDiff < closestTemperatureDiff) {
+            closestTemperatureDiff = temperatureDiff;
+            selectedBeerStyle = beer.name;
+            sortedBeerStyles = []; // Reset sortedBeerStyles array
+          } else if (temperatureDiff === closestTemperatureDiff) {
+            sortedBeerStyles.push(beer.name);
+          }
+        });
+      
+        if (!selectedBeerStyle) {
+          return res.status(404).json({ msg: 'No matching beer style found' });
+        }
+      
+        if (sortedBeerStyles.length > 0) {
+          sortedBeerStyles.push(selectedBeerStyle);
+          sortedBeerStyles.sort();
+          selectedBeerStyle = sortedBeerStyles[0];
+        }
+      
+        // Aqui você pode implementar a lógica para recuperar a playlist com base no estilo de cerveja selecionado.
+      
+        return res.json({ beerStyle: selectedBeerStyle });
       }
+      
       
 }
 
--- src/routes/beer.routes.js
+++ src/routes/beer.routes.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const BeerController = require('../controllers/beer.controller');
 
 
-router.get('/findAll',BeerController.findBeer);
+router.get('/seeAll',BeerController.findBeer);
 router.post('/create',BeerController.insertBeer);
 router.put('/update/:id', BeerController.updateBeer);
 router.delete('/delete/:id', BeerController.deleteBeer);
+router.post('/findBeer', BeerController.findBeerStyleByTemperature);
 
 module.exports = router;
